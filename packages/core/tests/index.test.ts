82f62ec18dc7207f24a9cbe2bf53583999ffcd11
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2023-11-23 14:49:42 +0800

    Add `FunctionDeclaration`

--- packages/core/tests/index.test.ts
+++ packages/core/tests/index.test.ts
@@ -191,3 +191,93 @@ describe('FunctionExpression', () => {
     });
   });
 });
+describe('FunctionDeclaration', () => {
+  describe('client', () => {
+    it('should transform valid server functions', async () => {
+      const code = `
+      async function example() {
+        'use server';
+        return 'foo bar';
+      }
+      `;
+      expect((await compiler.compile(ID, code, CLIENT)).code).toMatchSnapshot();
+    });
+    it('should skip non-async server functions', async () => {
+      const code = `
+      function example() {
+        'use server';
+        return 'foo bar';
+      }
+      `;
+      expect((await compiler.compile(ID, code, CLIENT)).code).toMatchSnapshot();
+    });
+    it('should transform valid server functions with scope', async () => {
+      const code = `
+      const outer = () => {
+        const value = 'foo bar';
+        async function example() {
+          'use server';
+          return value;
+        }
+      }
+      `;
+      expect((await compiler.compile(ID, code, CLIENT)).code).toMatchSnapshot();
+    });
+    it('should skip top-level values for scope', async () => {
+      const code = `
+      const value = 'foo bar';
+      const outer = () => {
+        async function example() {
+          'use server';
+          return value;
+        }
+      }
+      `;
+      expect((await compiler.compile(ID, code, CLIENT)).code).toMatchSnapshot();
+    });
+  });
+  describe('server', () => {
+    it('should transform valid server functions', async () => {
+      const code = `
+      async function example() {
+        'use server';
+        return 'foo bar';
+      }
+      `;
+      expect((await compiler.compile(ID, code, SERVER)).code).toMatchSnapshot();
+    });
+    it('should skip non-async server functions', async () => {
+      const code = `
+      function example() {
+        'use server';
+        return 'foo bar';
+      }
+      `;
+      expect((await compiler.compile(ID, code, SERVER)).code).toMatchSnapshot();
+    });
+    it('should transform valid server functions with scope', async () => {
+      const code = `
+      const outer = () => {
+        const value = 'foo bar';
+        async function example() {
+          'use server';
+          return value;
+        }
+      }
+      `;
+      expect((await compiler.compile(ID, code, SERVER)).code).toMatchSnapshot();
+    });
+    it('should skip top-level values for scope', async () => {
+      const code = `
+      const value = 'foo bar';
+      const outer = () => {
+        async function example() {
+          'use server';
+          return value;
+        }
+      }
+      `;
+      expect((await compiler.compile(ID, code, SERVER)).code).toMatchSnapshot();
+    });
+  });
+});
