f9d5c9b3293073262baeb4d90fc92f42950285a0
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2024-01-07 11:43:39 +0800

    Add `getDescriptiveName` support for methods

--- packages/core/compiler/index.ts
+++ packages/core/compiler/index.ts
@@ -39,27 +39,39 @@ function getImportIdentifier(
   return newID;
 }
 
-function getDescriptiveName(path: babel.NodePath): string {
+export function getDescriptiveName(path: babel.NodePath, defaultName: string): string {
   let current: babel.NodePath | null = path;
   while (current) {
     switch (current.node.type) {
       case 'FunctionDeclaration':
-      case 'FunctionExpression': {
+      case 'FunctionExpression':
         if (current.node.id) {
           return current.node.id.name;
         }
         break;
-      }
-      case 'VariableDeclarator': {
+      case 'VariableDeclarator':
         if (current.node.id.type === 'Identifier') {
           return current.node.id.name;
         }
         break;
-      }
+      case 'ClassPrivateMethod':
+      case 'ClassMethod':
+      case 'ObjectMethod':
+        switch (current.node.key.type) {
+          case 'Identifier':
+            return current.node.key.name;
+          case 'PrivateName':
+            return current.node.key.id.name;
+          default:
+            break;
+        }
+        break;
+      default:
+        break;
     }
     current = current.parentPath;
   }
-  return 'anonymous';
+  return defaultName;
 }
 
 function cleanServerBlockDirectives(
@@ -436,7 +448,7 @@ function transformServerBlock(
   // Create an ID
   let id = `${ctx.prefix}${ctx.count}`;
   if (ctx.options.env !== 'production') {
-    id += `-${getDescriptiveName(path)}`;
+    id += `-${getDescriptiveName(path, 'anonymous')}`;
   }
   ctx.count += 1;
   // Generate arguments for the server function instanciation
